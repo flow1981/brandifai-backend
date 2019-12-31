173d58a5812dd67d14f7c56982bf99809bcb0a4a
AlexShom <a.shomalistos@gmail.com>
2019-12-31 17:59:01 +0000

    Update profiles_controller.rb

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -22,36 +22,24 @@ class ProfilesController < ApplicationController
     # run scraper with profile name
     image_urls = Scraper.get_user_posts(profile_name, number)
  
-    # puts "lets print image urls"
-    # puts '--'
-    # puts image_urls
-    # puts '--'
-    # create images for each image url from scraper
-    # Image.new(|{imager_url})
+
     image_urls.each { |image_url| 
       image = Image.create({image_url: image_url, profile_id: profile.id})
-      callClarifaiApiOn(image, stat)
+      # callClarifaiApiOn(image, stat) //re enable pls
     }
 
-    # run Clarifai on each image
-    # create image deatil card for each image-ai-stat
     render json: params
 
   end
 
   def callClarifaiApiOn(image, stat)
 
-    puts image.id
-    array = []
-
     details = Clarifai.get_data(image.image_url)
 
     details.each{ |detail|
-      array << ImageDetail.create(image_id: image.id, stat_id: stat.id, tag: detail[:tag], value: detail[:value], model: detail[:model])
+      ImageDetail.create(image_id: image.id, stat_id: stat.id, tag: detail[:tag], value: detail[:value], model: detail[:model])
     }
     # ImageDetail.create(image_id: Image.first.id, stat_id: Stat.first.id, model: concept[:model], tag: concept[:tag], value: concept[:value])
-
-    puts array.size
     
   end
 
